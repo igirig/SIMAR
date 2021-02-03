79a8d23f87e6edcd6263dea3b07cec83e4d7a536
Luis Manuel Barragán Guerrero <177O00634@itsx.edu.mx>
2021-02-03 17:44:38 -0600

    v0.2

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -3,7 +3,6 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-use Haruncpi\LaravelIdGenerator\IdGenerator;
 
 class CreateUsersTable extends Migration
 {
@@ -15,13 +14,10 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $id = IdGenerator::generate(['table' => 'invoices', 'length' => 10, 'prefix' =>'INV-']);
-            $table->id('ID_User');
+            $table->id();
             $table->string('name');
-            $table->string('lastName');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->char('userType', 1);
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
